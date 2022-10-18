eeb20fe92218bb9d7664d8779874e47ef407de11
shofdahl <shofdahl@gmail.com>
2022-10-18 04:19:53 +0000

    updated HTML & CSS per assignment

--- scripts.js
+++ scripts.js
@@ -32,26 +32,6 @@ function loadFileInto(fromFile, whereTo) {
 
 window.onload = function() {
   
-  document.querySelector("#titleBanner h1").classList.add("tp");
-  
-  document.querySelector("#titleBanner h1").onclick = function() {    
-    this.classList.toggle("black");
-  }
-  
-  document.querySelector("#recipeColumns #ingredients").onclick = function() {
-    this.classList.toggle("tpShow")
-  }
-  
-  document.querySelector("#recipeColumns #equipment").onclick = function() {
-    this.classList.toggle("tpShow")
-  }
-  
-  document.querySelector("#recipeColumns #directions").onclick = function() {
-    this.classList.toggle("tpShow")
-  }
-  
-  document.querySelector("#recipeColumns #directions ol").innerHTML += "<li>Give to your friends for a sweet surprise.</li>";
-
-
+ 
   
 } //end window.onload
